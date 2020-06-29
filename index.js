a4251da226aa9d229bf4a74d44eecf70477accfc
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-29 12:30:17 +0100

    added auth configuration

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const auth = require('./auth');
 
 
 app.use(morgan('dev'));
