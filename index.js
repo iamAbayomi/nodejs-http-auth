d102962c7f3a28f9f5b132fb7c1e56469cf6c0ba
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-29 12:25:00 +0100

    Added middleware for authentication

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res)=> {
     res.statusCode = 200;
-    res.end("Welcome to your express app!")
+    res.end("Welcome to your express app! \n Making it work yeah r fd")
 })
 
+app.use(auth)
 app.get('/secret', (req,res) => {
     res.status = 200;
     res.end("*****");
