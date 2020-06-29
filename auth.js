65fbbe0a6e2e80fc5776ba2c928808f8dd870ac5
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-29 12:26:00 +0100

    exported authentication method *  Added validation for username and password

--- auth.js
+++ auth.js
@@ -1,7 +1,27 @@
 function auth(req, res, next){
     var authHeader = req.headers.authorization;
     if(!authHeader){
-        var err = new Error();
+        var err = new Error("Your are not authenticated");
+
+        res.setHeader("WWW-Authenticate", 'Basic');
+        err.status = 401
+        next(err)
     }
 
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var username = auth[0]
+    var password = auth[1]
+
+    if(username == 'edemone' && password == 'secret'){
+        next();
+    }else{
+        var err = new Error('Your are not authenticated')
+        
+        res.setHeader('WWW-Authenticate', 'Basic')
+        err.status = 401
+        next(err)
+    }
 }
+
+
+module.exports = auth;s
